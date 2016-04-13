9974f67b8823a25dde372e5bbfa32d042b881689
qstin <justinjeffreyp@gmail.com>
2016-04-13 13:58:48 -0700

    initial commit

---
+++ src/ai.js
@@ -0,0 +1,10 @@
+var AI = {
+  show: function(){
+    var mu = "<div class='fadeBox'> <div class='spinner'> </div></div>";
+    document.body.innerHTML += mu;
+  },
+  hide: function(){
+    $('.fadeBox').remove();
+
+  }
+}
